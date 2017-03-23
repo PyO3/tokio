c645b856f4e869ebd6d319796041510201e37d13
Nikolay Kim <fafhrd91@gmail.com>
2017-03-23 13:58:18 -0700

    use forked rust-cpython for right now

--- ext/src/lib.rs
+++ ext/src/lib.rs
@@ -3,6 +3,12 @@ extern crate tokio_core;
 #[macro_use] extern crate log;
 #[macro_use] extern crate cpython;
 
+#[cfg(feature="cpython/python27-sys")]
+extern crate python27_sys as ffi;
+
+#[cfg(feature="cpython/python3-sys")]
+extern crate python3_sys as ffi;
+
 mod handle;
 mod utils;
 mod event_loop;
@@ -11,9 +17,15 @@ use event_loop::{TokioEventLoop, new_event_loop, run_event_loop};
 
 py_module_initializer!(_ext, init_ext, PyInit__ext, |py, m| {
     m.add(py, "__doc__", "Asyncio event loop based on tokio")?;
-    try!(m.add_class::<TokioEventLoop>(py));
-    m.add(py, "new_event_loop", py_fn!(py, new_event_loop()))?;
-    m.add(py, "run_event_loop", py_fn!(py, run_event_loop(event_loop: &TokioEventLoop)))?;
 
+    init_tokio_module(py, m)?;
     Ok(())
 });
+
+
+pub fn init_tokio_module(py: cpython::Python, m: &cpython::PyModule) -> cpython::PyResult<()> {
+    m.add_class::<TokioEventLoop>(py)?;
+    m.add(py, "new_event_loop", py_fn!(py, new_event_loop()))?;
+    m.add(py, "run_event_loop", py_fn!(py, run_event_loop(event_loop: &TokioEventLoop)))?;
+    Ok(())
+}
