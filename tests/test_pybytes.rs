3b846c3d36fb6a3174eae95ec29ed798442b5717
Nikolay Kim <fafhrd91@gmail.com>
2017-04-27 16:08:38 -0700

    added basic test for pybytes

---
+++ tests/test_pybytes.rs
@@ -0,0 +1,49 @@
+#![allow(dead_code, unused_variables)]
+
+extern crate bytes;
+extern crate cpython;
+extern crate async_tokio;
+
+use cpython::*;
+use bytes::{Bytes, BytesMut};
+use async_tokio::PyBytes;
+
+
+macro_rules! py_run {
+    ($py:expr, $val:ident, $code:expr) => {{
+        let d = PyDict::new($py);
+        d.set_item($py, stringify!($val), &$val).unwrap();
+        $py.run($code, None, Some(&d)).expect($code);
+    }}
+}
+
+macro_rules! py_assert {
+    ($py:expr, $val:ident, $assertion:expr) => { py_run!($py, $val, concat!("assert ", $assertion)) };
+}
+
+
+#[test]
+fn test_pybytes() {
+    let gil = Python::acquire_gil();
+    let py = gil.python();
+
+    let bytestp = py.get_type::<PyBytes>();
+    // creating instances from python is not allowed
+    assert!(bytestp.call(py, NoArgs, None).is_err());
+
+    let bytes = Bytes::from("{\"test\": \"value\"}");
+    let pb = PyBytes::new(py, bytes).unwrap();
+
+    let d = PyDict::new(py);
+    d.set_item(py, "pb", pb).unwrap();
+    py.run("assert len(pb) == 17", None, Some(&d)).unwrap();
+    py.run("assert bytes(pb) == b'{\"test\": \"value\"}'", None, Some(&d)).unwrap();
+    py.run("assert str(pb, encoding=\"utf-8\") == '{\"test\": \"value\"}'",
+           None, Some(&d)).unwrap();
+    py.run("assert memoryview(pb) == b'{\"test\": \"value\"}'",
+           None, Some(&d)).unwrap();
+
+    // Fix in https://github.com/python/cpython/pull/1334
+    //py.run("import json; assert json.loads(pb) == {\"test\": \"value\"}",
+    //  None, Some(&d)).unwrap();
+}
