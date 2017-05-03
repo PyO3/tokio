dc1325b45c8d5037636daf2d02e2883107bd5dad
Nikolay Kim <fafhrd91@gmail.com>
2017-05-03 12:38:03 -0700

    base class for event loop policy

--- tokio/__init__.py
+++ tokio/__init__.py
@@ -1,4 +1,5 @@
-import asyncio  # noqa
+from asyncio.events import BaseDefaultEventLoopPolicy as __BasePolicy
+
 from . import _tokio
 
 __all__ = ('new_event_loop', 'EventLoopPolicy')
@@ -8,7 +9,7 @@ def new_event_loop():
     return _tokio.new_event_loop()
 
 
-class EventLoopPolicy:
+class EventLoopPolicy(__BasePolicy):
     """Event loop policy."""
 
     def _loop_factory(self):
