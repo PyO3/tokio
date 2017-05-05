886d23d37e71998f9e7b0ba691893d41181ca53c
Nikolay Kim <fafhrd91@gmail.com>
2017-05-05 11:32:33 -0700

    do not raise exception for setsockopt

--- src/socket.rs
+++ src/socket.rs
@@ -30,31 +30,31 @@ py_class!(pub class Socket |py| {
     }
 
     def accept(&self) -> PyResult<()> {
-        Err(PyErr::new::<exc::RuntimeError, _>(py, "Mthos is not supported."))
+        Err(PyErr::new::<exc::RuntimeError, _>(py, "accept method is not supported."))
     }
 
     def bind(&self, address) -> PyResult<()> {
-        Err(PyErr::new::<exc::RuntimeError, _>(py, "Mthos is not supported."))
+        Err(PyErr::new::<exc::RuntimeError, _>(py, "bind method is not supported."))
     }
 
     def close(&self) -> PyResult<()> {
-        Err(PyErr::new::<exc::RuntimeError, _>(py, "Mthos is not supported."))
+        Err(PyErr::new::<exc::RuntimeError, _>(py, "close method is not supported."))
     }
 
     def connect(&self, address) -> PyResult<()> {
-        Err(PyErr::new::<exc::RuntimeError, _>(py, "Mthos is not supported."))
+        Err(PyErr::new::<exc::RuntimeError, _>(py, "connect method is not supported."))
     }
 
     def connect_ex(&self, address) -> PyResult<()> {
-        Err(PyErr::new::<exc::RuntimeError, _>(py, "Mthos is not supported."))
+        Err(PyErr::new::<exc::RuntimeError, _>(py, "connect_ex method is not supported."))
     }
 
     def detach(&self) -> PyResult<()> {
-        Err(PyErr::new::<exc::RuntimeError, _>(py, "Mthos is not supported."))
+        Err(PyErr::new::<exc::RuntimeError, _>(py, "detach method is not supported."))
     }
 
     def dup(&self) -> PyResult<()> {
-        Err(PyErr::new::<exc::RuntimeError, _>(py, "Mthos is not supported."))
+        Err(PyErr::new::<exc::RuntimeError, _>(py, "dup method is not supported."))
     }
 
     def fileno(&self) -> PyResult<i32> {
@@ -62,14 +62,21 @@ py_class!(pub class Socket |py| {
     }
 
     def get_inheritable(&self) -> PyResult<()> {
-        Err(PyErr::new::<exc::RuntimeError, _>(py, "Mthos is not supported."))
+        Err(PyErr::new::<exc::RuntimeError, _>(py, "Method is not supported."))
     }
 
-    def getpeername(&self) -> PyResult<()> {
-        if let None = *self.peername(py) {
-            Err(PyErr::new::<exc::OSError, _>(py, "Socket is not connected"))
-        } else {
-            Err(PyErr::new::<exc::RuntimeError, _>(py, "Mthos is not supported."))
+    def getpeername(&self) -> PyResult<PyTuple> {
+        match *self.peername(py) {
+            None => Err(PyErr::new::<exc::OSError, _>(py, "Socket is not connected")),
+            Some(addr) => match addr {
+                SocketAddr::V4(addr) => {
+                    Ok((format!("{}", addr.ip()), addr.port()).to_py_tuple(py))
+                }
+                SocketAddr::V6(addr) => {
+                    Ok((format!("{}", addr.ip()),
+                        addr.port(), addr.flowinfo(), addr.scope_id(),).to_py_tuple(py))
+                },
+            }
         }
     }
 
@@ -86,96 +93,97 @@ py_class!(pub class Socket |py| {
     }
 
     def getsockopt(&self, *args, **kwargs) -> PyResult<()> {
-        Err(PyErr::new::<exc::RuntimeError, _>(py, "Mthos is not supported."))
+        Err(PyErr::new::<exc::RuntimeError, _>(py, "getsockopt method is not supported."))
     }
 
     def gettimeout(&self, *args, **kwargs) -> PyResult<()> {
-        Err(PyErr::new::<exc::RuntimeError, _>(py, "Mthos is not supported."))
+        Err(PyErr::new::<exc::RuntimeError, _>(py, "gettimeout method is not supported."))
     }
 
     def ioctl(&self, *args, **kwargs) -> PyResult<()> {
-        Err(PyErr::new::<exc::RuntimeError, _>(py, "Mthos is not supported."))
+        Err(PyErr::new::<exc::RuntimeError, _>(py, "ioctl method is not supported."))
     }
 
     def listen(&self, *args, **kwargs) -> PyResult<()> {
-        Err(PyErr::new::<exc::RuntimeError, _>(py, "Mthos is not supported."))
+        Err(PyErr::new::<exc::RuntimeError, _>(py, "listen method is not supported."))
     }
 
     def makefile(&self, *args, **kwargs) -> PyResult<()> {
-        Err(PyErr::new::<exc::RuntimeError, _>(py, "Mthos is not supported."))
+        Err(PyErr::new::<exc::RuntimeError, _>(py, "makefile method is not supported."))
     }
 
     def recv(&self, *args, **kwargs) -> PyResult<()> {
-        Err(PyErr::new::<exc::RuntimeError, _>(py, "Mthos is not supported."))
+        Err(PyErr::new::<exc::RuntimeError, _>(py, "recv method is not supported."))
     }
 
     def recvfrom(&self, *args, **kwargs) -> PyResult<()> {
-        Err(PyErr::new::<exc::RuntimeError, _>(py, "Mthos is not supported."))
+        Err(PyErr::new::<exc::RuntimeError, _>(py, "recvfrom method is not supported."))
     }
 
     def recvmsg(&self, *args, **kwargs) -> PyResult<()> {
-        Err(PyErr::new::<exc::RuntimeError, _>(py, "Mthos is not supported."))
+        Err(PyErr::new::<exc::RuntimeError, _>(py, "recvmsg method is not supported."))
     }
 
     def recvmsg_into(&self, *args, **kwargs) -> PyResult<()> {
-        Err(PyErr::new::<exc::RuntimeError, _>(py, "Mthos is not supported."))
+        Err(PyErr::new::<exc::RuntimeError, _>(py, "Method is not supported."))
     }
 
     def recvfrom_into(&self, *args, **kwargs) -> PyResult<()> {
-        Err(PyErr::new::<exc::RuntimeError, _>(py, "Mthos is not supported."))
+        Err(PyErr::new::<exc::RuntimeError, _>(py, "Method is not supported."))
     }
 
     def recv_into(&self, *args, **kwargs) -> PyResult<()> {
-        Err(PyErr::new::<exc::RuntimeError, _>(py, "Mthos is not supported."))
+        Err(PyErr::new::<exc::RuntimeError, _>(py, "Method is not supported."))
     }
 
     def send(&self, *args, **kwargs) -> PyResult<()> {
-        Err(PyErr::new::<exc::RuntimeError, _>(py, "Mthos is not supported."))
+        Err(PyErr::new::<exc::RuntimeError, _>(py, "send method is not supported."))
     }
 
     def sendall(&self, *args, **kwargs) -> PyResult<()> {
-        Err(PyErr::new::<exc::RuntimeError, _>(py, "Mthos is not supported."))
+        Err(PyErr::new::<exc::RuntimeError, _>(py, "sendall method is not supported."))
     }
 
     def sendto(&self, *args, **kwargs) -> PyResult<()> {
-        Err(PyErr::new::<exc::RuntimeError, _>(py, "Mthos is not supported."))
+        Err(PyErr::new::<exc::RuntimeError, _>(py, "sendto method is not supported."))
     }
 
     def sendmsg(&self, *args, **kwargs) -> PyResult<()> {
-        Err(PyErr::new::<exc::RuntimeError, _>(py, "Mthos is not supported."))
+        Err(PyErr::new::<exc::RuntimeError, _>(py, "sendmsg method is not supported."))
     }
 
     def sendmsg_afalg(&self, *args, **kwargs) -> PyResult<()> {
-        Err(PyErr::new::<exc::RuntimeError, _>(py, "Mthos is not supported."))
+        Err(PyErr::new::<exc::RuntimeError, _>(py, "sendmsg_afalg method is not supported."))
     }
 
     // we need to implement this
     def sendfile(&self, *args, **kwargs) -> PyResult<()> {
-        Err(PyErr::new::<exc::RuntimeError, _>(py, "Mthos is not supported."))
+        Err(PyErr::new::<exc::RuntimeError, _>(py, "sendfile method is not supported."))
     }
 
     def set_inheritable(&self, *args, **kwargs) -> PyResult<()> {
-        Err(PyErr::new::<exc::RuntimeError, _>(py, "Mthos is not supported."))
+        Err(PyErr::new::<exc::RuntimeError, _>(py, "Method is not supported."))
     }
 
     def setblocking(&self, *args, **kwargs) -> PyResult<()> {
-        Err(PyErr::new::<exc::RuntimeError, _>(py, "Mthos is not supported."))
+        Err(PyErr::new::<exc::RuntimeError, _>(py, "setblocking method is not supported."))
     }
 
     def settimeout(&self, *args, **kwargs) -> PyResult<()> {
-        Err(PyErr::new::<exc::RuntimeError, _>(py, "Mthos is not supported."))
+        Err(PyErr::new::<exc::RuntimeError, _>(py, "settimeout method is not supported."))
     }
 
     def setsockopt(&self, *args, **kwargs) -> PyResult<()> {
-        Err(PyErr::new::<exc::RuntimeError, _>(py, "Mthos is not supported."))
+        // Err(PyErr::new::<exc::RuntimeError, _>(py, "setsockopt method is not supported."))
+        Ok(())
     }
 
     def shutdown(&self, *args, **kwargs) -> PyResult<()> {
-        Err(PyErr::new::<exc::RuntimeError, _>(py, "Mthos is not supported."))
+        Err(PyErr::new::<exc::RuntimeError, _>(py, "shutdown method is not supported."))
     }
 
     def share(&self, *args, **kwargs) -> PyResult<()> {
-        Err(PyErr::new::<exc::RuntimeError, _>(py, "Mthos is not supported."))
+        Err(PyErr::new::<exc::RuntimeError, _>(py, "share method is not supported."))
     }
 
 });
